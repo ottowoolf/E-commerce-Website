c288eca96c855afc39dd2b4c5ea28f234f42a6a8
Otto Wolf <otto.woolf@gmail.com>
2021-03-07 01:04:49 +1100

    modified api keys for firebase and stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const publishableKey =
+    "pk_test_51IESVaFyshLsn7PDNA81BzcveC3zUZKf5DX3LkWhBPsGkyO9oFV2JONYHIVF2xseLrmwIBGzqIMDUeBB1PFkpWc4001pJPPIUY";
 
-  const onToken = token => {
+  const onToken = (token) => {
     axios({
-      url: 'payment',
-      method: 'post',
+      url: "payment",
+      method: "post",
       data: {
         amount: priceForStripe,
-        token: token
-      }
+        token: token,
+      },
     })
-      .then(response => {
-        alert('succesful payment');
+      .then((response) => {
+        alert("succesful payment");
       })
-      .catch(error => {
-        console.log('Payment Error: ', JSON.parse(error));
+      .catch((error) => {
+        console.log("Payment Error: ", JSON.parse(error));
         alert(
-          'There was an issue with your payment! Please make sure you use the provided credit card.'
+          "There was an issue with your payment! Please make sure you use the provided credit card."
         );
       });
   };
